f866837ba729175af31c7e4ee960ac13eb728b69
Ra1nyyday <bkimadieff@gmail.com>
2023-12-20 22:46:02 +0600

    Added expiration date in warehouseProduct model

--- backend/warehouse/models.py
+++ backend/warehouse/models.py
@@ -17,9 +17,10 @@ class WarehouseProduct(models.Model):
     date_added = models.DateTimeField(default = now)
     quantity = models.IntegerField()
     price_per_unit = models.FloatField()
-    total_price = models.FloatField()
+    total_price = models.FloatField(blank = True)
     status = models.CharField(max_length = 40, choices = Status.choices, default = Status.AVAILABLE)
     
     company = models.ForeignKey(Company, on_delete = models.CASCADE)
     receiver_staff = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    expiration_date = models.DateTimeField(default = now)
