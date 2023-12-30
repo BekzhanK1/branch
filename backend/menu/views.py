834e3b5d0454633c7e0aae1fd069a6466720975b
uamirk <amirussembayev@gmail.com>
2023-12-30 23:18:00 +0600

    Making url and views for menu

--- backend/menu/views.py
+++ backend/menu/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, MenuItem
+from .serializers import CategorySerializer, MenuItemSerializer
+from rest_framework.decorators import api_view   
+from rest_framework.response import Response
+# it should be working, but it isn't
+# class CategoryViewSet(viewsets.ModelViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
 
-# Create your views here.
+# class MenuItemViewSet(viewsets.ModelViewSet):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+@api_view(['GET'])
+
+def apiOverviewCategory(request): 
+    api_urls = {
+    'List':' /category-list/',
+    'Detail View': ' /category-detail/<str:pk>/',
+    'Create':' /category-create/',
+    'Update': '/category-update',
+    'Delete': '/category-delete'
+    }
+    return Response(api_urls)
+def apiOverviewMenuItems(request): 
+    api_urls = {
+    'List':' /menu-items-list/',
+    'Detail View': ' /menu-items-detail/<str:pk>/',
+    'Create':' /menu-items-create/',
+    'Update': '/menu-items-update',
+    'Delete': '/menu-items-delete'
+    }
+    return Response(api_urls)
+
